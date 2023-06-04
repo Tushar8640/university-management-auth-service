f4b499b9f8da74306ab13bf2df59422b1d6ef37c
Tushar@8640 <imtantushar44@gmail.com>
2023-06-04 14:41:05 +0600

    cors and parser completed

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, urlencoded } from "express";
+import cors from "cors";
 const app: Application = express();
 
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
 
-
-
-export default app
+export default app;
