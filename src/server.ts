cfb752849f7e942e2f55d155fd3b6c7edbbee4f8
Tushar@8640 <imtantushar44@gmail.com>
2023-06-04 14:37:28 +0600

    setup complete

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index"
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("database connected successfully");
+
+    app.listen(config.port, () => {
+      console.log(`App connected on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Error happned", error);
+  }
+}
+
+bootstrap();
